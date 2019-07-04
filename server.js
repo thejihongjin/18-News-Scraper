6a1996598de5c09360c65c74e5d52176559e0557
thejihongjin <46304180+thejihongjin@users.noreply.github.com>
2019-07-04 13:33:24 -0700

    added get route

--- server.js
+++ server.js
@@ -17,23 +17,25 @@ app.use(express.static("public"));
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI);
 
-axios.get("https://www.bbc.com/sport/football/womens").then(function (response) {
-    var $ = cheerio.load(response.data);
-    var results = [];
-
-    $(".lakeside__content").each(function (i, element) {
-        var title = $(element).children().children().children("span").text();
-        var summary = $(element).children("p").text() || "N/A";
-        var link = $(element).children().children().attr("href");
-        if (!link.includes("https://www.bbc.co")) {
-            link = "https://www.bbc.com" + link;
-        }
-
-        results.push({
-            title: title,
-            summary: summary,
-            link: link
+app.get("/scrape", function (req, res) {
+    axios.get("https://www.bbc.com/sport/football/womens").then(function (response) {
+        var $ = cheerio.load(response.data);
+        var results = [];
+    
+        $(".lakeside__content").each(function (i, element) {
+            var title = $(element).children().children().children("span").text();
+            var summary = $(element).children("p").text() || "N/A";
+            var link = $(element).children().children().attr("href");
+            if (!link.includes("https://www.bbc.co")) {
+                link = "https://www.bbc.com" + link;
+            }
+    
+            results.push({
+                title: title,
+                summary: summary,
+                link: link
+            });
         });
+        console.log(results);
     });
-    console.log(results);
 });
