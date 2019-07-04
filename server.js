df213d91ce0622f3621b0a04b2fe4095959be622
thejihongjin <46304180+thejihongjin@users.noreply.github.com>
2019-07-04 13:41:57 -0700

    updated to saving result as object inside .each function

--- server.js
+++ server.js
@@ -20,22 +20,16 @@ mongoose.connect(MONGODB_URI);
 app.get("/scrape", function (req, res) {
     axios.get("https://www.bbc.com/sport/football/womens").then(function (response) {
         var $ = cheerio.load(response.data);
-        var results = [];
     
         $(".lakeside__content").each(function (i, element) {
-            var title = $(element).children().children().children("span").text();
-            var summary = $(element).children("p").text() || "N/A";
-            var link = $(element).children().children().attr("href");
-            if (!link.includes("https://www.bbc.co")) {
-                link = "https://www.bbc.com" + link;
+            var result = {};
+            result.title = $(element).children().children().children("span").text();
+            result.summary = $(element).children("p").text() || "N/A";
+            result.link = $(element).children().children().attr("href");
+            if (!result.link.includes("https://www.bbc.co")) {
+                result.link = "https://www.bbc.com" + result.link;
             }
-    
-            results.push({
-                title: title,
-                summary: summary,
-                link: link
-            });
+            // console.log(result);
         });
-        console.log(results);
     });
 });
