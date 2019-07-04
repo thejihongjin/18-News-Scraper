11edeb19c760c393898b1fe33a012f5bf3d1d9c4
thejihongjin <46304180+thejihongjin@users.noreply.github.com>
2019-07-04 13:32:42 -0700

    required models, added port, initialized express and configured middleware, connected to mongodb

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ var mongoose = require("mongoose");
 var cheerio = require("cheerio");
 var axios = require("axios");
 
+var db = require("./models");
+
+// var PORT = 3000;
+
+var app = express(); // initializes Express
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
+
 axios.get("https://www.bbc.com/sport/football/womens").then(function (response) {
     var $ = cheerio.load(response.data);
     var results = [];
